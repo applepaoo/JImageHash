0bf68f309e969b388c5dd46a16ee90ce0e653fe0
applepaoo <applepaoo@gmail.com>
2018-08-14 16:34:45 +0800

    modified

--- src/main/JImageHashPro.java
+++ src/main/JImageHashPro.java
@@ -115,6 +115,8 @@ public class JImageHashPro {
 				continue;
 			} catch (StringIndexOutOfBoundsException e) {
 				continue;
+			} catch (Exception e) {
+				continue;
 			}
 
 		}
@@ -139,11 +141,11 @@ public class JImageHashPro {
 
 			// System.out.println(imagePath);
 			Date current = new Date();
-			HashingAlgorithm hasher = new AverageHash(bitResolution);
+			//HashingAlgorithm hasher = new AverageHash(bitResolution);
 			HashingAlgorithm hasher2 = new PerceptiveHash(bitResolution);
 			BufferedImage img = (BufferedImage) ImageIO.read(file2);
 			String jimagePHash = String.valueOf(hasher2.hash(img).toString(16));
-			String jimageAHash = String.valueOf(hasher.hash(img).toString(16));
+			//String jimageAHash = String.valueOf(hasher.hash(img).toString(16));
 
 			// jphash
 			RadialHash hash = jpHash.getImageRadialHash("/mnt/" + imagePath);
@@ -152,10 +154,10 @@ public class JImageHashPro {
 			// output json
 			jsonObject1.put("G_NO", String.valueOf(jsonObject.get("G_NO")));
 			jsonObject1.put("_SOUTCE_TIME", sdf.format(current));
-			jsonObject1.put("HASH_IMG", imageName + "_s." + str);
+			jsonObject1.put("HASH_IMG", imageName);
 			jsonObject1.put("IMG_HASH_V1", jpHash); // jphash
 			jsonObject1.put("IMG_HASH_V2", jimagePHash); // JImageHash P_hash
-			jsonObject1.put("IMG_HASH_V3", jimageAHash); // JImageHash A_hash
+			//jsonObject1.put("IMG_HASH_V3", jimageAHash); // JImageHash A_hash
 			fw.write(jsonObject1.toString() + "\r\n");
 			System.out.println(sdf.format(current));
 			System.out.println(String.valueOf(file2)); // 印出路徑
